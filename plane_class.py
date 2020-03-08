70b481f40baf2d93c4b9daf0594cc01d5e546e6b
abdullah ayyaz <abdullahayyaz@tcadmins-air.home>
2020-03-08 15:47:09 +0000

    created plan class and added methods

---
+++ plane_class.py
@@ -0,0 +1,15 @@
+# import my vehicle class
+from vehicles_class import *
+
+# define plane class here and make it inherit from vehicle
+class plane(vehicles):
+
+    def __init__(self, n_passengers, cargo):
+        self.n_passengers = n_passengers
+        self.cargo = cargo
+
+    def take_off(selfs):
+        return 'take off'
+    def touch_down(self):
+        return 'we have touched down'
+    
